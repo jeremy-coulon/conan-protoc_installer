33bbdc4bd01c7efa9dfaa3bf1f5f40ff4843e56b
Jérémy Coulon <jeremy.coulon.jrmc@gmail.com>
2019-02-26 17:54:44 +0100

    Don't use CMake for tests

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+
 from conans import ConanFile, CMake
 
 
 class TestPackageConan(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
-    build_requires = "cmake_installer/[>=2.8.11]@conan/stable"
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure()
-        cmake.build()
+    settings = "os", "arch"
+    exports = "addressbook.proto"
 
     def test(self):
         self.run("protoc --version", run_environment=True)
+        addressbook = os.path.join(self.source_folder, "addressbook.proto")
+        self.run("protoc --cpp_out=. --java_out=. --proto_path={source_folder} {file}".format(source_folder=self.source_folder, file=addressbook), run_environment=True)
